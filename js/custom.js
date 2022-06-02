aa0ce177599d6070a18af1bf07a6f341b4003c98
NitishLeenIO <101103599+NitishLeenIO@users.noreply.github.com>
2022-06-02 12:09:33 +0530

    projects added

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,132 @@
-$(window).load(function() {
+$(window).load(function () {
+  $("#carousel").flexslider({
+    animation: "slide",
+    controlNav: true,
+    animationLoop: false,
+    slideshow: true,
+    itemWidth: 350,
+    itemMargin: 30,
+    asNavFor: "#slider",
+    touch: true,
+    mousewheel: true,
+    customDirectionNav: $(".custom-navigation a"),
+  });
 
-    $('#carousel').flexslider({
-        animation: "slide",
-        controlNav: true,
-        animationLoop: false,
-        slideshow: true,
-        itemWidth: 380,
-        itemMargin: 30,
-        asNavFor: '#slider',
-        touch:true,
-        mousewheel: true, 
-        controlsContainer: $(".custom-controls-container"),
-    customDirectionNav: $(".custom-navigation a")
-      });
+
+  $("#carousel1").flexslider({
+    animation: "slide",
+    controlNav: true,
+    animationLoop: false,
+    slideshow: true,
+    itemWidth: 350,
+    itemMargin: 30,
+    asNavFor: "#slider",
+    touch: true,
+    mousewheel: true,
+    customDirectionNav: $(".custom-navigation1 a"),
+  });
+
+  $("#c2").flexslider({
+    animation: "slide",
+    controlNav: true,
+    animationLoop: false,
+    slideshow: true,
+    itemWidth: 350,
+    itemMargin: 30,
+    asNavFor: "#slider",
+    touch: true,
+    mousewheel: true,
+    customDirectionNav: $(".cn2 a"),
+  });
+
+  $("#c3").flexslider({
+    animation: "slide",
+    controlNav: true,
+    animationLoop: false,
+    slideshow: true,
+    itemWidth: 350,
+    itemMargin: 30,
+    asNavFor: "#slider",
+    touch: true,
+    mousewheel: true,
+    customDirectionNav: $(".cn3 a"),
+  });
+
+
+  $("#c4").flexslider({
+    animation: "slide",
+    controlNav: true,
+    animationLoop: false,
+    slideshow: true,
+    itemWidth: 350,
+    itemMargin: 30,
+    asNavFor: "#slider",
+    touch: true,
+    mousewheel: true,
+    customDirectionNav: $(".cn4 a"),
+  });
 
   
-})
+  $("#c5").flexslider({
+    animation: "slide",
+    controlNav: true,
+    animationLoop: false,
+    slideshow: true,
+    itemWidth: 350,
+    itemMargin: 30,
+    asNavFor: "#slider",
+    touch: true,
+    mousewheel: true,
+    customDirectionNav: $(".cn5 a"),
+  });
+
+    
+  $("#c6").flexslider({
+    animation: "slide",
+    controlNav: true,
+    animationLoop: false,
+    slideshow: true,
+    itemWidth: 350,
+    itemMargin: 30,
+    asNavFor: "#slider",
+    touch: true,
+    mousewheel: true,
+    customDirectionNav: $(".cn6 a"),
+  });
+
+
+  $("#c7").flexslider({
+    animation: "slide",
+    controlNav: true,
+    animationLoop: false,
+    slideshow: true,
+    itemWidth: 350,
+    itemMargin: 30,
+    asNavFor: "#slider",
+    touch: true,
+    mousewheel: true,
+    customDirectionNav: $(".cn7 a"),
+  });
+
+
+  
+  $("#c8").flexslider({
+    animation: "slide",
+    controlNav: true,
+    animationLoop: false,
+    slideshow: true,
+    itemWidth: 350,
+    itemMargin: 30,
+    asNavFor: "#slider",
+    touch: true,
+    mousewheel: true,
+    customDirectionNav: $(".cn8 a"),
+  });
+
+
+
+
+
+
+
+});
